3f6500fb84ddd73cfc07cc69e45af6d8ad7eb6dc
inhyeok <i_bc.kim@samsung.com>
2015-06-12 20:35:42 +0900

    Merge based tizen 2.4 + experiment
    [Version] Release 0.1.2
    [Profile] Common
    [Issue Type]Refactoring
    
    Change-Id: I3e0a764078ae99dccb286115b776ca209715a942

--- include/wav_player.h
+++ include/wav_player.h
@@ -14,9 +14,6 @@
 * limitations under the License. 
 */
 
-
-
-
 #ifndef __TIZEN_MEDIA_WAV_PLAYER_H__
 #define __TIZEN_MEDIA_WAV_PLAYER_H__
 
@@ -28,11 +25,9 @@ extern "C"
 {
 #endif
 
-#define WAV_PLAYER_ERROR_CLASS          TIZEN_ERROR_MULTIMEDIA_CLASS | 0x50
-
 /**
  * @file wav_player.h
- * @brief This file contains the WAV player API
+ * @brief This file contains the WAV player API.
  */
 
 /**
@@ -41,62 +36,85 @@ extern "C"
  */
 
 /**
- * @brief Enumerations of error code for WAV player.
+ * @brief Enumeration of error codes for WAV player.
+ * @since_tizen @if MOBILE 2.3 @elseif WEARABLE 2.3.1 @endif
  */
 typedef enum
 {
-    WAV_PLAYER_ERROR_NONE        = TIZEN_ERROR_NONE,                      /**< Successful */
-    WAV_PLAYER_ERROR_INVALID_PARAMETER = TIZEN_ERROR_INVALID_PARAMETER,       /**< Invalid parameter */
-    WAV_PLAYER_ERROR_INVALID_OPERATION = TIZEN_ERROR_INVALID_OPERATION,       /**< Invalid operation */
-    WAV_PLAYER_ERROR_FORMAT_NOT_SUPPORTED = WAV_PLAYER_ERROR_CLASS | 0x01 			  /**< Not supported format */
+    WAV_PLAYER_ERROR_NONE                 = TIZEN_ERROR_NONE,                  /**< Successful */
+    WAV_PLAYER_ERROR_INVALID_PARAMETER    = TIZEN_ERROR_INVALID_PARAMETER,     /**< Invalid parameter */
+    WAV_PLAYER_ERROR_INVALID_OPERATION    = TIZEN_ERROR_INVALID_OPERATION,     /**< Invalid operation */
+    WAV_PLAYER_ERROR_NOT_SUPPORTED        = TIZEN_ERROR_NOT_SUPPORTED,         /**< Not supported (Since 2.4) */
+    WAV_PLAYER_ERROR_FORMAT_NOT_SUPPORTED = TIZEN_ERROR_WAV_PLAYER | 0x01      /**< Format not supported */
 } wav_player_error_e;
 
 /**
- * @}
- */
-
-
-/**
- * @addtogroup CAPI_MEDIA_WAV_PLAYER_MODULE
- * @{
- */
-
-/**
- * @brief Called when a WAV file has finished playing.
+ * @brief Called when a WAV file is no longer being played.
  * @details This callback is not invoked by calling wav_player_stop().
- *
+ * @since_tizen @if MOBILE 2.3 @elseif WEARABLE 2.3.1 @endif
  * @param[in] id	The completed wav player ID
  * @param[in] user_data	The user data passed from the callback registration function
  * @see wav_player_start()
  */
 typedef void (*wav_player_playback_completed_cb)(int id, void *user_data);
 
-
 /**
+ * @deprecated Deprecated since 2.4. Use wav_player_start_with_stream_info() instead.
  * @brief Plays a WAV file.
- * @param[in] path	The WAV file path to play
- * @param[in] type	The sound type
- * @param[in] callback	The callback function to invoke when a WAV file is finished playing
- * @param[in] user_data	The user data to be passed to the callback function
- * @param[out] id	The WAV player ID ( can be set to NULL )
+ * @since_tizen @if MOBILE 2.3 @elseif WEARABLE 2.3.1 @endif
  *
- * @return 0 on success, otherwise a negative error value.
+ * @remarks Sound can be mixed with other sounds if you don't control the sound session in sound-manager module since 2.4.\n
+ * 	You can refer to @ref CAPI_MEDIA_SUOND_MANAGER_MODULE.
+ *
+ * @param[in] path	The file path to play
+ * @param[in] type	The sound type
+ * @param[in] callback	The callback function to be invoked when a WAV file is no longer being played
+ * @param[in] user_data	The user data to be passed to the callback function
+ * @param[out] id	The WAV player ID (can be set to @c NULL)
+ *
+ * @return @c 0 on success,
+ *         otherwise a negative error value
  * @retval #WAV_PLAYER_ERROR_NONE Successful
- * @retval #WAV_PLAYER_ERROR_INVALID_PARAMETER Invalid parameter 
+ * @retval #WAV_PLAYER_ERROR_INVALID_PARAMETER Invalid parameter
  * @retval #WAV_PLAYER_ERROR_INVALID_OPERATION Invalid operation
  *
- * @post	It invokes wav_player_playback_completed_cb() when a WAV file has finished playing.
+ * @post	It invokes wav_player_playback_completed_cb() when a WAV file is no longer being played.
  * @see wav_player_stop()
  * @see wav_player_playback_completed_cb()
  */
 int wav_player_start(const char *path, sound_type_e type, wav_player_playback_completed_cb callback, void *user_data, int *id);
 
 /**
+ * @brief Plays a WAV file with stream information of sound-manager.
+ * @since_tizen 2.4
+ * @param[in] path	The file path to play
+ * @param[in] stream_info	The sound stream information handle
+ * @param[in] callback	The callback function to be invoked when a WAV file is no longer being played
+ * @param[in] user_data	The user data to be passed to the callback function
+ * @param[out] id	The WAV player ID (can be set to @c NULL)
+ *
+ * @return @c 0 on success,
+ *         otherwise a negative error value
+ * @retval #WAV_PLAYER_ERROR_NONE Successful
+ * @retval #WAV_PLAYER_ERROR_INVALID_PARAMETER Invalid parameter
+ * @retval #WAV_PLAYER_ERROR_INVALID_OPERATION Invalid operation
+ * @retval #WAV_PLAYER_ERROR_NOT_SUPPORTED Not supported
+ *
+ * @post	It invokes wav_player_playback_completed_cb() when a WAV file is no longer being played.
+ * @see wav_player_stop()
+ * @see wav_player_playback_completed_cb()
+ * @see sound_manager_create_stream_information()
+ * @see sound_manager_destroy_stream_information()
+ */
+int wav_player_start_with_stream_info(const char *path, sound_stream_info_h stream_info, wav_player_playback_completed_cb callback, void *user_data, int *id);
+
+/**
  * @brief Stops playing the WAV file.
+ * @since_tizen @if MOBILE 2.3 @elseif WEARABLE 2.3.1 @endif
+ * @param[in] id  The WAV player ID to stop
  *
- * @param[in] id	The WAV player ID to stop
- *
- * @return 0 on success, otherwise a negative error value.
+ * @return @c 0 on success, 
+ *         otherwise a negative error value
  * @retval #WAV_PLAYER_ERROR_NONE Successful
  * @retval #WAV_PLAYER_ERROR_INVALID_PARAMETER Invalid parameter 
  * @retval #WAV_PLAYER_ERROR_INVALID_OPERATION Invalid operation  
