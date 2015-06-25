7b656e59c90810bdd765229775694fef32a017a3
inhyeok <i_bc.kim@samsung.com>
2015-06-25 17:17:07 +0900

    Merge based 2.4
    [Version] Release 0.1.17
    [Profile] Common
    [Issue Type] Migration
    
    Merged main changes
    - Apply ASLR
    - Adapt stream-based routing
    
    Change-Id: I67d23611f2728290e6f271b0c275523921113064
    Signed-off-by: inhyeok <i_bc.kim@samsung.com>

--- src/wav_player.c
+++ src/wav_player.c
@@ -11,12 +11,14 @@
 * distributed under the License is distributed on an "AS IS" BASIS,
 * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 * See the License for the specific language governing permissions and
-* limitations under the License. 
+* limitations under the License.
 */
 
 
 #define LOG_TAG "CAPI_MEDIA_WAV_PLAYER"
 
+#include <sound_manager.h>
+#include <sound_manager_internal.h>
 #include <mm_sound.h>
 #include <mm_sound_private.h>
 #include <stdio.h>
@@ -74,6 +76,59 @@ int wav_player_start(const char *path, sound_type_e type, wav_player_playback_co
 		free(cb_data);
 
 	return __convert_wav_player_error_code(__func__, ret);
+}
+
+int wav_player_start_with_stream_info(const char *path, sound_stream_info_h stream_info, wav_player_playback_completed_cb cb, void *user_data, int *id)
+{
+	int ret = MM_ERROR_NONE;
+	int player = -1;
+	char m_path[PATH_MAX];
+	void (*_completed_cb)(void *, int);
+	_completed_cb = NULL;
+	_cb_data *cb_data = NULL;
+	char *stream_type = NULL;
+	int stream_id;
+
+	if( path == NULL)
+		return __convert_wav_player_error_code(__func__, WAV_PLAYER_ERROR_INVALID_PARAMETER);
+
+	ret = sound_manager_get_type_from_stream_information(stream_info, &stream_type);
+	if( ret )
+		return __convert_wav_player_error_code(__func__, ret);
+	ret = sound_manager_get_index_from_stream_information(stream_info, &stream_id);
+	if( ret )
+		return __convert_wav_player_error_code(__func__, ret);
+
+	m_path[0] = '\0';
+	if( path[0] != '/' ){
+
+		if( getcwd(m_path, PATH_MAX) != NULL){
+			strncat(m_path, "/",PATH_MAX-strlen(m_path) );
+		}
+	}
+	strncat(m_path, path, PATH_MAX-strlen(m_path));
+
+	if( cb ){
+		_completed_cb = __internal_complete_cb;
+		cb_data = (_cb_data *)malloc(sizeof(_cb_data));
+		if(cb_data == NULL )
+			return __convert_wav_player_error_code(__func__, WAV_PLAYER_ERROR_INVALID_OPERATION);
+		cb_data->cb = cb;
+		cb_data->user_data = user_data;
+	}
+
+
+	ret = mm_sound_play_sound_with_stream_info(m_path, stream_type, stream_id, _completed_cb , cb_data, &player);
+
+	if( ret == 0 && id != NULL){
+		*id = player;
+	}
+
+	if( ret != 0 && cb_data != NULL)
+		free(cb_data);
+
+	return __convert_wav_player_error_code(__func__, ret);
+
 }
 
 int wav_player_stop(int id)
