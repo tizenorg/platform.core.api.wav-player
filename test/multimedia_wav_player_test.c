50b1d85e67d18670749c08e2db22c4c6f4d51486
Seungbae Shin <seungbae.shin@samsung.com>
2016-04-01 19:32:41 +0900

    Initial code for muse migration
    Change-Id: I9dc6215d16e0c432f960f36a1ba503880995f24a

--- test/multimedia_wav_player_test.c
+++ test/multimedia_wav_player_test.c
@@ -24,19 +24,12 @@
 #include <stdlib.h>
 #include <getopt.h>
 
-#define FILE_PATH_MAX   30
+#define FILE_PATH_MAX   256
 #define DEFAULT_FILE    "test.wav"
 
 static GMainLoop *g_mainloop = NULL;
 static GThread *event_thread;
-
-gpointer GmainThread(gpointer data)
-{
-	g_mainloop = g_main_loop_new(NULL, 0);
-	g_main_loop_run(g_mainloop);
-
-	return NULL;
-}
+int id;
 
 void help()
 {
@@ -50,25 +43,45 @@ void help()
 void _player_stop_cb(int id, void *user_data)
 {
 	printf("complete id = %d,%d\n", id, (int)user_data);
+
+	g_main_loop_quit(g_mainloop);
+}
+
+void _sound_stream_focus_state_changed_cb(sound_stream_info_h stream_info, sound_stream_focus_change_reason_e reason_for_change, const char *additional_info, void *user_data)
+{
+	printf("stream_info = %d, reason = %d, additional_info = %s, user_data = %p\n", stream_info, reason_for_change, additional_info, user_data);
 }
 
 
 void wav_play_test(const char* file_path, int iterate)
 {
 	int ret = 0;
-	int id;
+
+	sound_stream_info_h stream_info;
+
 	int i;
 	if (iterate <= 0 || file_path == NULL) {
 		printf("invalid param : %d\n", time);
 		return;
 	}
 
+	sound_manager_create_stream_information(SOUND_STREAM_TYPE_SYSTEM, _sound_stream_focus_state_changed_cb, NULL, &stream_info);
+
 	printf("Play Wav, File Path : %s, Iterate : %d\n", file_path, iterate);
 	for (i = 0 ; i < iterate; i++) {
-		ret = wav_player_start(file_path, SOUND_TYPE_MEDIA, _player_stop_cb, (void*)i, &id);
-		printf("wav_player_start(%d)(id=%d) ret = %d\n", i, id, ret);
+	//	ret = wav_player_start(file_path, SOUND_TYPE_MEDIA, _player_stop_cb, (void*)i, &id);
+		ret = wav_player_start_with_stream_info(file_path, stream_info, _player_stop_cb, (void*)i, &id);
+		printf("wav_player_start(%d)(id=%d) ret = 0x%x\n", i, id, ret);
 
 	}
+}
+
+static gboolean _timeout_cb(gpointer user_data)
+{
+	int ret = wav_player_stop(id);
+	printf("wav_player_stop(id=%d) ret = 0x%x\n", id, ret);
+	g_main_loop_quit(g_mainloop);
+	return FALSE;
 }
 
 
@@ -104,9 +117,13 @@ int main(int argc, char**argv)
 		}
 	}
 
-	event_thread = g_thread_new("WavPlayerTest", GmainThread, NULL);
+	g_mainloop = g_main_loop_new(NULL, 0);
 
 	wav_play_test(file_path, iterate);
+
+	g_timeout_add(5000, _timeout_cb, NULL);
+
+	g_main_loop_run(g_mainloop);
 
 	return 0;
 }
